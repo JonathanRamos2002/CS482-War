961c549c2bd2fac421f9facab35475b9014d7c65
AYOPOSI O <aolu-bamisaye@loyola.edu>
2024-10-31 22:45:08 -0400

    added lobby placeholder

---
+++ sprint_two/src/components/Lobby.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Lobby = () => {
+  return (
+    <div className="lobby-container">
+      <h2>Welcome to the Lobby!</h2>
+    </div>
+  );
+};
+
+export default Lobby;
