eb441bdfb5eb144807f2ef86b81ac38793702798
AYOPOSI O <aolu-bamisaye@loyola.edu>
2024-11-02 11:36:04 -0400

    added firebase components to lobby

--- sprint_two/src/components/Lobby.js
+++ sprint_two/src/components/Lobby.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserIcon, PlusCircle, Users } from 'lucide-react';
+import { getFirestore, collection, addDoc, onSnapshot,doc,updateDoc, deleteDoc, query,orderBy} from 'firebase/firestore';
+import './Lobby.css';
 
-const Lobby = () => {
+const Lobby = ({ user, isGuest, guestUsername }) => {
+  const navigate = useNavigate();
+  const [tables, setTables] = useState([]);
+  const db = getFirestore();
+
+
   return (
     <div className="lobby-container">
       <h2>Welcome to the Lobby!</h2>
