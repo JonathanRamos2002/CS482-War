e8a6670a1af4baf83e0f6ef24453a904569996c7
AYOPOSI O <aolu-bamisaye@loyola.edu>
2024-11-02 11:44:09 -0400

    added function for getting user name and guest name

--- sprint_two/src/components/Lobby.js
+++ sprint_two/src/components/Lobby.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserIcon, PlusCircle, Users } from 'lucide-react';
+import { getFirestore, collection, addDoc, onSnapshot,doc,updateDoc, deleteDoc, query,orderBy} from 'firebase/firestore';
+import './Lobby.css';
 
-const Lobby = () => {
+
+const Lobby = ({ user, isGuest, guestUsername }) => {
+  const navigate = useNavigate();
+  const [tables, setTables] = useState([]);
+  const db = getFirestore();
+  
+  const getCurrentUserId = () => {
+    if (isGuest) {
+
+      return `guest-${guestUsername}`; // gets guest name
+    }
+    return user?.uid;
+  };
+
+  const getCurrentUsername = () => { // gets user name
+    if (isGuest) {
+      return guestUsername;
+    }
+    return user?.email;
+  };
+
   return (
     <div className="lobby-container">
       <h2>Welcome to the Lobby!</h2>
